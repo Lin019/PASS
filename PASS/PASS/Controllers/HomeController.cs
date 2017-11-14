48a6cdfd35c548a92f21f0a7e55c8db6227aa433
dev_lin <voyage01090@gmail.com>
2017-11-14 14:49:47 +0800

    Revert "Merge branch 'master_new'"
    This reverts commit 853b415c555a0f174205af289dba54c96d43fa77.

--- PASS/PASS/Controllers/HomeController.cs
+++ PASS/PASS/Controllers/HomeController.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using PASS.Services;
+
 
 namespace PASS.Controllers
 {
     public class HomeController : Controller
     {
+        public MemberService _memberService = new MemberService();
         public ActionResult Index()
         {
+<<<<<<< HEAD
             ViewBag.Title = "登入";
 
             return View();
@@ -21,8 +26,30 @@ namespace PASS.Controllers
             return View();
         }
         public ActionResult Assignment()
-        {
+=======
             return View();
+        }
+
+        //新增帳號
+        [HttpPost]
+        public JsonResult CreateUser(string id, string account, string password, string name, string email, int type)
+>>>>>>> 54a66c37b625e9f97fe300895684fb95e332c762
+        {
+            /*string id, string account, string password, string name, string email, int type*/
+            /*string  id = "998";
+            string account = "FUCK";
+            string password = "123";
+            string  name = "55";
+            string email = "123@456";
+            int type = 1;*/
+            return Json(_memberService.CreateUser(id, account, password, name, email, type));
+        }
+        //測試 取得會員資料
+        [HttpPost]
+        public JsonResult GetMemberInfo()
+        {
+
+            return Json(_memberService.GetMemberInfo());
         }
     }
 }
