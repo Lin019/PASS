3edc9bfe34376cdcb7e1bb94bcef80719ae5722e
LAPTOP-3UACUNMC\User <likesm0887@yahoo.com.tw>
2017-11-23 01:03:03 +0800

    新增作業 MODEL 新增作業 新增作業單元測試

---
+++ PASS/PASS/Dao/AssignmentDao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Web.Configuration;
+using PASS.Models;
+
+namespace PASS.Dao
+{
+    public class AssignmentDao
+    {
+        private string GetDBConnectionString()
+        {
+            return WebConfigurationManager.ConnectionStrings["PASSDatabase"].ConnectionString;
+        }
+        public string CreateAssignment(int assignmentId, string assignmentName, string assignmentDescription, string assignmentFormat, DateTime assignmentDeadline,bool assignmentLate, string courseId)
+        {
+            string sql = @"INSERT INTO assignment (assignment_ID , assignment_Name,assignment_Description, assignment_Format,  assignment_Deadline,assignment_Late,course_ID) VALUES (@assignmentID , @assignmentName,@assignmentDescription, @assignmentFormat, @assignmentDeadline,@assignmentLate,@courseId)";
+            using (var connection = new MySqlConnection(GetDBConnectionString()))
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                cmd.Parameters.AddWithValue("@assignmentID", assignmentId);
+                cmd.Parameters.AddWithValue("@assignmentName", assignmentName);
+                cmd.Parameters.AddWithValue("@assignmentDescription", assignmentDescription);
+                cmd.Parameters.AddWithValue("@assignmentFormat", assignmentFormat);
+                cmd.Parameters.AddWithValue("@assignmentDeadline", assignmentDeadline);
+                cmd.Parameters.AddWithValue("@assignmentLate", assignmentLate);
+                cmd.Parameters.AddWithValue("@courseId", courseId);
+                try
+                {
+                    cmd.ExecuteReader(); //execure the reader
+                    return "success";
+                }
+                catch (Exception e)
+                {
+                    return "fail";
+                    throw e;
+                }
+               
+            }
+
+        }
+    }
+}
---
+++ PASS/PASSTests/Dao/AssignmentDaoTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PASS.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PASS.Dao.Tests
+{
+    [TestClass()]
+    public class AssignmentDaoTests
+    {
+        AssignmentDao _AssignmentDao = new AssignmentDao();
+        [TestMethod()]
+        public void CreateUserTest()
+        {
+
+            _AssignmentDao.CreateAssignment(2, "作業系統_HW1", "排班計算", "ZIP", DateTime.Now, false, "1");
+
+        }
+    }
+}
