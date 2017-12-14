68f921e584af651a6e7178b46f284b130f4126cc
Lai <100643828@gmail.com>
2017-12-14 14:36:59 +0800

    新增解壓縮到一資料夾

--- PASS/PASS/Services/AssignmentUploadService.cs
+++ PASS/PASS/Services/AssignmentUploadService.cs
@@ -14,22 +14,22 @@ namespace PASS.Services
         private AssignmentDao _assignmentDao = new AssignmentDao();
         private SubmitDao _submitDao = new SubmitDao();
         //上傳作業
-        public void UploadAssignment(string studentID,int assignmentID, HttpPostedFileBase file)
+        public void UploadAssignment(string studentID, int assignmentID, HttpPostedFileBase file)
         {
             Assignment assignment = _assignmentDao.GetOneAssignment(assignmentID);
-            if(DateTime.Compare(assignment._assignmentDeadline,DateTime.Now)<0)
+            if (DateTime.Compare(assignment._assignmentDeadline, DateTime.Now) < 0)
             {
                 if (!assignment._assignmentLate) throw new Exception("Time out");
             }
-            
+
             if (file != null)
             {
                 if (file.ContentLength > 0)
                 {
-                    if (Path.GetExtension(file.FileName) != "."+assignment._assignmentFormat.ToLower()) throw new Exception("File extension wrong");
-                    var fileName = studentID+"_" + assignment._assignmentName + Path.GetExtension(file.FileName);
-                    string MyDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) ;
-                    string passPath = "\\PASS" + "\\" + assignment._courseId.ToString() + "\\" + assignment._assignmentName;
+                    if (Path.GetExtension(file.FileName) != "." + assignment._assignmentFormat.ToLower()) throw new Exception("File extension wrong");
+                    var fileName = studentID + "_" + assignment._assignmentName + Path.GetExtension(file.FileName);
+                    string MyDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    string passPath = "\\PASS" + "\\" + assignment._courseId.ToString() + "\\" + assignment._assignmentName+"\\";
                     MyDocumentsPath += passPath;
                     if (!Directory.Exists(MyDocumentsPath))
                         Directory.CreateDirectory(@MyDocumentsPath);
@@ -42,9 +42,24 @@ namespace PASS.Services
                 throw new Exception("File isn't selected");
         }
         //下載作業資訊
-        public SubmitInfo DownloadAssignmentInfo(string studentID,int assignmentID)
+        public SubmitInfo DownloadAssignmentInfo(string studentID, int assignmentID)
         {
             return _submitDao.GetOneSubmitInfo(studentID, assignmentID);
+        }
+        //將作業解壓縮到一資料夾
+        public string UnzipIntoFolder(string studentID,int assignmentID)
+        {
+            SubmitInfo submit = DownloadAssignmentInfo(studentID, assignmentID);
+            string MyDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string filePath = MyDocumentsPath + submit._submitUrl + submit._submitName;
+            String assetName = Path.GetFileNameWithoutExtension(submit._submitName);
+            string zipPath = MyDocumentsPath + submit._submitUrl+"Unzip\\" + assetName;
+            //System.IO.Compression.ZipFile.CreateFromDirectory(filePath, zipPath);
+            if (!Directory.Exists(filePath)) throw new Exception("File not found");
+            if (!Directory.Exists(zipPath)) Directory.CreateDirectory(@zipPath);
+            System.IO.Compression.ZipFile.ExtractToDirectory(filePath, zipPath);
+            //↑有可能出現檔案重複Exception
+            return zipPath;
         }
     }
 }
