1fef34042b4e0bbbf956b4b63cb4b6536de37cac
Bonislolz <ttiimm2334225@gmail.com>
2017-12-24 21:05:30 +0800

    新增查詢學生個人該課程所有作業繳交狀況

--- PASS/PASSTests/Services/AssignmentUploadServiceTests.cs
+++ PASS/PASSTests/Services/AssignmentUploadServiceTests.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PASS.Services;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.UI;
 
 namespace PASS.Services.Tests
 {
@@ -17,6 +13,22 @@ namespace PASS.Services.Tests
         {
             _assignmentUploadService.UnzipIntoFolder(1024);
             Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOneStudentSubmitStatusListTest()
+        {
+            _assignmentUploadService = new AssignmentUploadService();
+            List<Pair> actual = _assignmentUploadService.GetOneStudentSubmitStatusList("103590034", 1);
+            List<Pair> expect = new List<Pair>();
+            expect.Add(new Pair(1024, "已繳交"));
+            expect.Add(new Pair(1044, "未繳交"));
+            expect.Add(new Pair(1046, "未繳交"));
+            for (int i = 0; i < actual.Count; i++)
+            {
+                Assert.AreEqual(actual[i].First, expect[i].First);
+                Assert.AreEqual(actual[i].Second, expect[i].Second);
+            }
         }
     }
 }
